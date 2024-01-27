263a6365e279c30a233e84a3a9f891cf6e6b2b56
Indrarjun <billasaketh@gmail.com>
2024-01-27 21:43:44 +0530

    Stage 02 Data Validatiton Completed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from textSummerizer.pipleline.stage_01_data_ingestion import (
     DataIngestionTrainingPipeline,
 )
+from textSummerizer.pipleline.stage_02_data_validation import (
+    DataValidationTraningPipeline,
+)
 from textSummerizer.logging import logger
 
 
@@ -9,6 +12,17 @@ try:
     logger.info(f">>>> Stage {STAGE_NAME} Started <<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<< \n\nx==============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>> Stage {STAGE_NAME} Started <<<<<")
+    data_validation = DataValidationTraningPipeline()
+    data_validation.main()
     logger.info(f">>>> stage {STAGE_NAME} completed <<<<< \n\nx==============x")
 except Exception as e:
     logger.exception(e)
